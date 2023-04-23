4313a3909e1b9dff2a3b8fdccdfceb43920dbb46
Supriya More <supriya0050@gmail.com>
2023-04-23 02:02:41 +0200

    Modified HTTP request type for future scope perspective

--- src/main/java/com/example/project/controller/SignalHandlerController.java
+++ src/main/java/com/example/project/controller/SignalHandlerController.java
@@ -2,8 +2,8 @@ package com.example.project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +18,8 @@ public class SignalHandlerController {
 	@Autowired
 	public SignalService signalService;
 	
-	@GetMapping("/{signal}")
-	public ResponseEntity<String> handleSignal(@PathVariable("signal") int signal) {
+	@PostMapping
+	public ResponseEntity<String> handleSignal(@RequestBody int signal) {
 		signalService.handleSignal(signal);
 		return new ResponseEntity<>("Dummy rest call", HttpStatus.OK);
 	}
--- src/test/java/com/example/project/controller/SingleHandlerControllerTest.java
+++ src/test/java/com/example/project/controller/SingleHandlerControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.project.controller;
 
-import org.springframework.http.MediaType;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -9,13 +10,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.example.project.service.SignalService;
 
-@ExtendWith(MockitoExtension.class)
+
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SingleHandlerControllerTest {
@@ -26,12 +31,17 @@ public class SingleHandlerControllerTest {
 	@Autowired
 	private MockMvc mockmvc;
 	
+	@MockBean 
+	private SignalService signalService;
+	
 	@Test
 	public void handleSignalAPI() throws Exception {
-		mockmvc.perform(MockMvcRequestBuilders.get("/signals/1")
-	            .accept(MediaType.APPLICATION_JSON))
+		mockmvc.perform(MockMvcRequestBuilders.post("/signals").content("1")
+	            .contentType(MediaType.APPLICATION_JSON))
 	            .andExpect(MockMvcResultMatchers.status().isOk())
 	            .andExpect(MockMvcResultMatchers.content().string("Dummy rest call"))
 	            .andDo(MockMvcResultHandlers.print());
+		
+		verify(signalService, times(1)).handleSignal(1);
 	}
 }
